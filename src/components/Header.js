939c492c93336b20eaa7ce1575590691856144b1
Paloma St.Clair <palomast.clair@Palomas-MacBook-Pro.local>
2022-10-21 13:21:12 -0400

    added components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(){
+    return (
+        <div>
+            <p>gaslight from home</p>
+        </div>
+    );
+}
+export default Header;
---
+++ src/components/SideBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SideBar(){
+    return (
+        <div>
+            <p>gaslight from the side</p>
+        </div>
+    );
+}
+export default SideBar;
